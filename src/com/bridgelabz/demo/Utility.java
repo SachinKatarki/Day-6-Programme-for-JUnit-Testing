98a0a4ec264a25e3ea8d527cf4ac028f796e51ef
Textspot <sachinkatarki1995@gmail.com>
2021-11-07 13:15:11 +0530

    Added:Temperature conversion programme

---
+++ src/com/bridgelabz/demo/Utility.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.demo;
+
+public class Utility {
+
+	public static boolean isNumber(String in) {
+		
+		return false;
+	}
+
+	public static String celsiusToFahrenheit(double temp) {
+		
+		return null;
+	}
+
+	public static String fahrenheitToCelsius(double temp1) {
+		
+		return null;
+	}
+
+}
