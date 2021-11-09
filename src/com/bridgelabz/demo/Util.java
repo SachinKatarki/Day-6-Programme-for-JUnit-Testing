416c3a4de9c47bcd67f1220903208ce73d0bab7f
Textspot <sachinkatarki1995@gmail.com>
2021-11-09 20:07:15 +0530

    Added:Welcome to binary programme

---
+++ src/com/bridgelabz/demo/Util.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.demo;
+
+public class Util {
+
+	public static int[] toBinary(int nextInt) {
+		return null;
+	}
+
+	public static int toDecimal(int[] binary) {
+	
+		return 0;
+	}
+
+}
